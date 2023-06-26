13f08075d7cbcb96bd5cf383e2dd0db23f078977
abonstrom-smartbear <81653121+abonstrom-smartbear@users.noreply.github.com>
2023-06-26 15:40:49 -0400

    Update and rename product.repository.js to payment.repository.js

--- src/product/payment.repository.js
+++ src/product/payment.repository.js
@@ -1,12 +1,12 @@
-const Product = require('./product');
+const Product = require('./payment');
 
 class ProductRepository {
 
     constructor() {
         this.products = new Map([
-            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", 99.99)],
-            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", 49.49)],
-            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", 16.50)],
+            ["09", new Payment("09", "CREDIT_CARD", "Gem Visa", "v1", 99.99)],
+            ["10", new Payment("10", "CREDIT_CARD", "28 Degrees", "v1", 49.49)],
+            ["11", new Payment("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", 16.50)],
         ]);
     }
 
