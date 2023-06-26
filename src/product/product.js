679c0f51f7182695c6a252c3bb4fff87cd33fe99
abonstrom-smartbear <81653121+abonstrom-smartbear@users.noreply.github.com>
2023-06-26 15:38:03 -0400

    Update product.js

--- src/product/product.js
+++ src/product/product.js
@@ -1,10 +1,10 @@
-class Product {
-    constructor(id, type, name, version, price) {
+class Payment {
+    constructor(id, name, version, price, account) {
         this.id = id;
-        this.type = type;
         this.name = name;
         this.version = version;
         this.price = price;
+        this.account = account;
     }
 }
 
