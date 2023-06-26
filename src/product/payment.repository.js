2184f82725ea95f182e643f5a728067c97c20743
abonstrom-smartbear <81653121+abonstrom-smartbear@users.noreply.github.com>
2023-06-26 15:44:05 -0400

    Update payment.repository.js

--- src/product/payment.repository.js
+++ src/product/payment.repository.js
@@ -3,24 +3,24 @@ const Product = require('./payment');
 class ProductRepository {
 
     constructor() {
-        this.products = new Map([
-            ["09", new Payment("09", "CREDIT_CARD", "Gem Visa", "v1", 99.99)],
-            ["10", new Payment("10", "CREDIT_CARD", "28 Degrees", "v1", 49.49)],
-            ["11", new Payment("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", 16.50)],
+        this.payments = new Map([
+            ["09", new Payment("09", "Payment 09", "v1", 99.99, "Customer 1")],
+            ["10", new Payment("10", "Payment 10", "v1", 49.49, "Customer 2")],
+            ["11", new Payment("11", "PAyment 11", "v2", 16.50, "Customer 3")],
         ]);
     }
 
     async fetchAll() {
-        return [...this.products.values()]
+        return [...this.payments.values()]
     }
 
     async getById(id) {
-        return this.products.get(id);
+        return this.payments.get(id);
     }
 
-    async create(product) {
-        return this.products.set(product.id, product)
+    async create(payment) {
+        return this.payments.set(payment.id, payment)
     }
 }
 
-module.exports = ProductRepository;
+module.exports = PaymentRepository;
